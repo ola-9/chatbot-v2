340723c09922b73f54985c018a6c21b2714a7ef3
ola-9 <nepogodda@gmail.com>
2024-01-16 14:03:26 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'
+import getWidget from './init.tsx';
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const run = (): void => {
+  const Widget = getWidget();
+  const root = document.getElementById('root');
+  ReactDOM.render(Widget, root);
+
+  // ReactDOM.createRoot(document.getElementById('root')!).render(
+  //   <React.StrictMode>
+  //     <Widget />
+  //   </React.StrictMode>,
+  // )
+
+};
+
+run();
