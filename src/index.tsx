5dd34738f218cffcee44b38e436aa4111bd39188
ola-9 <nepogodda@gmail.com>
2024-01-16 14:22:24 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom'
 import getWidget from './init.tsx';
 
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 const run = (): void => {
   const Widget = getWidget();
   const root = document.getElementById('root');
   ReactDOM.render(Widget, root);
-
-  // ReactDOM.createRoot(document.getElementById('root')!).render(
-  //   <React.StrictMode>
-  //     <Widget />
-  //   </React.StrictMode>,
-  // )
-
 };
 
 run();
--- src/init.tsx
+++ src/init.tsx
@@ -1,4 +1,5 @@
-import App from "./App";
+// import App from "./App";
+import App from '@dzencot/chatbot-v2';
 
 export default () => {
   return <App />;
